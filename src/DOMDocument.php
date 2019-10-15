45333ab961209dc6f40427e0c590888cb413540b
napoleon101392 <napoleon101392@gmail.com>
2019-10-15 18:44:43 +0800

    bug fix: debugging

--- src/DOMDocument.php
+++ src/DOMDocument.php
@@ -118,8 +118,7 @@ class DOMDocument
 
             return trim(preg_replace('/\n/', '', $content));
         } catch (\Exception $e) {
-
-            return null;
+            return '';
         }
     }
 
